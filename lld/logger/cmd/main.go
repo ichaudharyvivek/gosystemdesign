dc0f4643da11f64f348b544788a1bb8441c95e6d
Vivek Chaudhary <51666451+ichaudharyvivek@users.noreply.github.com>
2026-04-12 07:48:12 +0000

    feat: fixes

--- lld/logger/cmd/main.go
+++ lld/logger/cmd/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
-	logger "lld-logger/internal/logger"
+	"lld-logger/internal/logger"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
+	TestSimple()
+	// TestConcurrent()
+}
+
+func TestSimple() {
 	log := logger.New()
 
-	// Writting logs
 	log.Debug().Msg("This is a debug message")
 	log.Info().Fields(map[string]any{"type": "info", "stackName": "ANT_UAT"}).Msg("This is an info messsage")
 	log.Warn().Msg("This is a warning message")
 	log.Error().Msg("This is an error message")
 	log.Fatal().Msg("This is a fatal message")
+}
+
+func TestConcurrent() {
+	// Run with: go run -race main.go
+	log := logger.New()
+
+	var g errgroup.Group
+	for i := 0; i < 100; i++ {
+		i := i
+		g.Go(func() error {
+			log.Info().Fields(map[string]any{
+				"id": i,
+			}).Msg("concurrent log")
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		panic(err)
+	}
 }
--- lld/logger/go.mod
+++ lld/logger/go.mod
@@ -1,3 +1,5 @@
 module lld-logger
 
 go 1.25.4
+
+require golang.org/x/sync v0.20.0
